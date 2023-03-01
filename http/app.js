2acb5fcae1e881e7b0b58c4943ad7cd6592ab666
c2kaka <c2kaka@126.com>
2023-03-01 17:24:19 +0800

    feat: ssr

--- http/app.js
+++ http/app.js
@@ -1,6 +1,8 @@
 const Server = require("./server-simple");
 const Router = require("./router");
 const param = require("./aspects/param");
+const fs = require('fs');
+const handlebars = require("handlebars");
 
 const app = new Server();
 const router = new Router();
@@ -16,17 +18,34 @@ app.use(
   router.get("/coronavirus/index", async ({ res }, next) => {
     const { getCoronavirusKeyIndex } = require("./mock/mock");
     const index = getCoronavirusKeyIndex();
-    res.setHeader("Content-Type", "application/json");
-    res.body = { data: index };
+
+    const handlebars = require('handlebars');
+    const html = fs.readFileSync('./www/pages/covoid19.html', { encoding: 'utf-8' });
+    const tpl = handlebars.compile(html);
+    const body = tpl({data: index});
+
+    res.setHeader("Content-Type", "text/html");
+    res.body = body;
     await next();
   })
 );
 
-app.use(router.get('/coronavirus/:date', async ({route, res}, next) => {
+app.use(router.get('/coronavirus/:date', async ({params, route, res}, next) => {
     const {getCoronavirusByDate} = require('./mock/mock');
     const data = getCoronavirusByDate(route.date);
-    res.setHeader('Content-Type', 'application/json');
-    res.body = {data};
+
+    if (params.type === 'json') { // url like this: http://localhost:8080/coronavirus/2020-01-25?type=json
+        res.setHeader('Content-Type', 'application/json');
+        res.body = {data};
+    } else {
+        const handlebars = require('handlebars');
+        const html = fs.readFileSync('./www/pages/covoid-date.html', { encoding: 'utf-8' });
+        const tpl = handlebars.compile(html);
+        const body = tpl({data});
+
+        res.setHeader('Content-Type', 'text/html');
+        res.body = body;
+    }
     await next();
 }));
 
