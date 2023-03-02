3aab67b0e795d87a34e104fade72d77c2e9e9238
c2kaka <c2kaka@126.com>
2023-03-02 10:14:50 +0800

    feat: param middleware support POST method

--- todolist/server/aspects/param.js
+++ todolist/server/aspects/param.js
@@ -2,9 +2,33 @@ const url = require("url");
 const querystring = require("querystring");
 
 module.exports = async function (ctx, next) {
+  // parse url query
   const { req } = ctx;
   const { query } = url.parse(`http://${req.headers.host}${req.url}`);
   ctx.params = querystring.parse(query);
+
+  // parse post
+  if (req.method === 'POST') {
+    // get body
+    const body = await new Promise((resolve) => {
+      let res = '';
+      req.on('data', chunk => {
+        res = chunk + res;
+      });
+
+      req.on('end', () => {
+        resolve(res);
+      })
+    });
+
+    ctx.params = ctx.params || {};
+    const headers = req.headers;
+    if (headers['content-type'] === 'application/x-www-form-urlencoded') {
+      Object.assign(ctx.params, querystring.parse(body));
+    } else if (headers['content-type'] === 'application/json') {
+      Object.assign(ctx.params, JSON.parse(body));
+    }
+  }
 
   await next();
 };
