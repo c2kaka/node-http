0f5932377c0b01c5e1613066d8f13c38733502d0
c2kaka <c2kaka@126.com>
2023-03-02 10:44:42 +0800

    feat: router wildcard

--- todolist/server/index.js
+++ todolist/server/index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const sqlite3 = require('sqlite3');
 const {open} = require('sqlite');
 const path = require('path');
+const url = require('url');
+const zlib = require('zlib');
+const mime = require("mime");
 
 const app = new Server();
 const router = new Router();
@@ -44,6 +47,75 @@ app.use(router.post('/add', async ({database, params, res}, next) => {
     const { addTask } = require('./model/todolist');
     const data = await addTask(database, params);
     res.body = { data };
+    await next();
+}));
+
+app.use(router.all('.*', async ({params, req, res}, next) => {
+    let filePath = path.resolve(__dirname, path.join('../www', url.fileURLToPath(`file:///${req.url}`)));
+
+    if(fs.existsSync(filePath)) {
+        const stats = fs.statSync(filePath);
+        if(stats.isDirectory()) {
+            filePath = path.join(filePath, 'index.html');
+        }
+        if(fs.existsSync(filePath)) {
+            const {ext} = path.parse(filePath);
+            const stats = fs.statSync(filePath);
+            const timeStamp = req.headers['if-modified-since'];
+            res.statusCode = 200;
+            if(timeStamp && Number(timeStamp) === stats.mtimeMs) {
+                res.statusCode = 304;
+            }
+            const mimeType = mime.getType(ext);
+            res.setHeader('Content-Type', mimeType);
+            res.setHeader('Cache-Control', 'max-age=86400');
+            res.setHeader('Last-Modified', stats.mtimeMs);
+            const acceptEncoding = req.headers['accept-encoding'];
+            const compress = acceptEncoding && /^(text|application)\//.test(mimeType);
+            let compressionEncoding;
+            if(compress) {
+                acceptEncoding.split(/\s*,\s*/).some((encoding) => {
+                    if(encoding === 'gzip') {
+                        res.setHeader('Content-Encoding', 'gzip');
+                        compressionEncoding = encoding;
+                        return true;
+                    }
+                    if(encoding === 'deflate') {
+                        res.setHeader('Content-Encoding', 'deflate');
+                        compressionEncoding = encoding;
+                        return true;
+                    }
+                    if(encoding === 'br') {
+                        res.setHeader('Content-Encoding', 'br');
+                        compressionEncoding = encoding;
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            if(res.statusCode === 200) {
+                const fileStream = fs.createReadStream(filePath);
+                if(compress && compressionEncoding) {
+                    let comp;
+                    if(compressionEncoding === 'gzip') {
+                        comp = zlib.createGzip();
+                    } else if(compressionEncoding === 'deflate') {
+                        comp = zlib.createDeflate();
+                    } else {
+                        comp = zlib.createBrotliCompress();
+                    }
+                    res.body = fileStream.pipe(comp);
+                } else {
+                    res.body = fileStream;
+                }
+            }
+        }
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.body = '<h1>Not Found</h1>';
+        res.statusCode = 404;
+    }
+
     await next();
 }));
 
